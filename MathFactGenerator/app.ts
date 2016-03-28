59f04336074ea671dc8ccfe7cf15f1abf027c4cd
agr <andrey.grigoriev@gmail.com>
2016-03-27 23:05:15 -0700

    Excluded .sln More structure in the code. Initial generator configuration interface

--- MathFactGenerator/app.ts
+++ MathFactGenerator/app.ts
@@ -3,76 +3,145 @@
     maxDigits: number;
     minValue: number;
     maxValue: number;
+
+    generate(): number {
+        var min: number = -999999999;
+        if (typeof this.minDigits === 'number') {
+            min = Math.pow(10, this.minDigits - 1);
+        }
+        if (typeof this.minValue === 'number') {
+            min = Math.max(min, this.minValue);
+        }
+
+        var max: number = 999999999;
+        if (typeof this.maxDigits === 'number') {
+            max = Math.pow(10, this.maxDigits);
+        }
+        if (typeof this.maxValue === 'number') {
+            max = Math.min(max, this.maxValue);
+        }
+
+        return min + Math.floor(Math.random() * (max - min));
+    }
 }
 
 interface FactGenerator {
-    (operands: Array<NumberDescription>, result: NumberDescription): HTMLElement;
+    createConfigurationElement(): HTMLElement;
+    generateFact(): HTMLElement;
 }
 
-function generateNumber(descr: NumberDescription): number {
-    var min: number = -999999999;
-    if (typeof descr.minDigits === 'number') {
-        min = Math.pow(10, descr.minDigits - 1);
-    }
-    if (typeof descr.minValue === 'number') {
-        min = Math.max(min, descr.minValue);
+class GeneratorManager {
+    private generators: Array<FactGenerator>;
+
+    constructor() {
+        this.generators = new Array<FactGenerator>();
     }
 
-    var max: number = 999999999;
-    if (typeof descr.maxDigits === 'number') {
-        max = Math.pow(10, descr.maxDigits);
-    }
-    if (typeof descr.maxValue === 'number') {
-        max = Math.min(max, descr.maxValue);
+    addGenerator(generator: FactGenerator): void {
+        this.generators.push(generator);
     }
 
-    return min + Math.floor(Math.random() * (max - min));
+    buildConfigurator(): void {
+        var cfgRoot: HTMLElement = document.getElementById('configuration');
+        for (var i = 0; i < this.generators.length; ++i) {
+            var n: HTMLElement = this.generators[i].createConfigurationElement();
+            cfgRoot.appendChild(n);
+        }
+    }
+
+    generateFacts(): void {
+        var resultRoot: HTMLElement = document.getElementById('facts');
+        for (var i = 0; i < 20; ++i) {
+            var idx: number = Math.floor(Math.random() * this.generators.length);
+            var n: HTMLElement = this.generators[idx].generateFact();
+            resultRoot.appendChild(n);
+        }
+    }
 }
 
-function GenerateLongMultiplication(operands: Array<NumberDescription>, result: NumberDescription): HTMLElement {
-    var container: HTMLElement = document.createElement('div');
-    container.className = 'fact-container';
+class LongMultiplicationGenerator implements FactGenerator {
+    leftOperandDescription: NumberDescription;
+    rightOperandDescription: NumberDescription;
+    resultDescription: NumberDescription;
 
-    var maxDigits: number = 0;
+    constructor() {
+        this.leftOperandDescription = new NumberDescription();
+        this.rightOperandDescription = new NumberDescription();
+        this.resultDescription = new NumberDescription();
+    }
 
-    for (var i: number = 0; i < operands.length; ++i) {
+    createConfigurationElement(): HTMLElement {
+        var tle: HTMLElement = document.createElement('div');
+        tle.className = 'configuration long-multiplication';
+
+        tle.appendChild(this.operandConfiguration('Left operand', this.leftOperandDescription));
+        tle.appendChild(this.operandConfiguration('Right operand', this.rightOperandDescription));
+        tle.appendChild(this.operandConfiguration('Result', this.resultDescription));
+
+        return tle;
+    }
+
+    private operandConfiguration(caption: string, opDesc: NumberDescription): HTMLElement {
+        var opCfg: HTMLElement = document.createElement('div');
+        opCfg.innerHTML = '<div>' + caption + '</div><div>Digits: <input type="number" class="min-digits">&ndash;<input type="number" class="max-digits"></div><div>Value: <input type="number" class="min-value">&ndash;<input type="number" class="max-value"></div>';
+
+        var minDigits: HTMLElement = <HTMLElement>opCfg.getElementsByClassName('min-digits')[0];
+        var maxDigits: HTMLElement = <HTMLElement>opCfg.getElementsByClassName('max-digits')[0];
+        var minValue: HTMLElement = <HTMLElement>opCfg.getElementsByClassName('min-value')[0];
+        var maxValue: HTMLElement = <HTMLElement>opCfg.getElementsByClassName('max-value')[0];
+        var _this: LongMultiplicationGenerator = this;
+        minDigits.onblur = function () { opDesc.minDigits = this.value == '' ? undefined : parseInt(this.value); };
+        maxDigits.onblur = function () { opDesc.maxDigits = this.value == '' ? undefined : parseInt(this.value); };
+        minValue.onblur = function () { opDesc.minValue = this.value == '' ? undefined : parseInt(this.value); };
+        maxValue.onblur = function () { opDesc.maxValue = this.value == '' ? undefined : parseInt(this.value); };
+
+        return opCfg;
+    }
+
+    generateFact(): HTMLElement {
+        var container: HTMLElement = document.createElement('div');
+        container.className = 'fact-container';
+
+        var maxDigits: number = 0;
+
         var div: HTMLElement = document.createElement('div');
         container.appendChild(div);
         div.className = 'operand';
-        var n: number = generateNumber(operands[i]);
-        var nd: number = ('' + n).length;
-        if (nd > maxDigits) {
-            maxDigits = nd;
-        }
-        var prefix: string = '';
-        if (i == 1) {
-            prefix = '&times;'
-        }
+        var n: number = this.leftOperandDescription.generate();
+        var nd: number = n.toString().length;
+        maxDigits = Math.max(maxDigits, nd);
+        var r = n;
+        div.innerHTML = n.toString();
 
-        div.innerHTML = prefix + n;
-    }
+        div = document.createElement('div');
+        container.appendChild(div);
+        div.className = 'operand';
+        n = this.rightOperandDescription.generate();
+        r *= n;
+        nd = n.toString().length;
+        maxDigits = Math.max(maxDigits, nd);
+        div.innerHTML = '&times;' + n;
 
-    var hr = document.createElement('hr');
-    container.appendChild(hr);
-    var spc = document.createElement('div');
-    spc.innerHTML = '&nbsp;';
-    container.appendChild(spc);
+        var hr = document.createElement('hr');
+        container.appendChild(hr);
+        var spc = document.createElement('div');
+        spc.innerHTML = '&nbsp;';
+        container.appendChild(spc);
 
-    container.style.width = '' + (maxDigits + 1) + 'em';
+        container.style.width = r.toString().length.toString() + 'em';
 
-    return container;
-}
-
-function generate(parent: HTMLElement) {
-    var operands: Array<NumberDescription> = [<NumberDescription>{ minDigits: 3, maxDigits: 3 }, <NumberDescription>{ minDigits: 2, maxDigits: 2 }];
-    for (var i = 0; i < 10; ++i) {
-        var el = GenerateLongMultiplication(operands, <NumberDescription>{});
-        parent.appendChild(el);
+        return container;
     }
 }
 
 window.onload = () => {
     var el = document.getElementById('content');
     var f = document.getElementById('facts');
-    generate(f);
+    var genBtn = document.getElementById('btn-generate');
+
+    var gm: GeneratorManager = new GeneratorManager();
+    gm.addGenerator(new LongMultiplicationGenerator());
+    gm.buildConfigurator();
+
+    genBtn.onclick = () => { gm.generateFacts(); };
 };
