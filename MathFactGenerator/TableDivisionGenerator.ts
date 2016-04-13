3281d6945753ca041175dbda2df9b52af56d3266
agr <andrey.grigoriev@gmail.com>
2016-04-12 21:10:38 -0700

    Added table division generator

---
+++ MathFactGenerator/TableDivisionGenerator.ts
@@ -0,0 +1,35 @@
+﻿import { FactGenerator } from './FactGenerator';
+import { BasicNumberGenerator } from './BasicNumberGenerator';
+
+export class TableDivisionGenerator implements FactGenerator {
+    gen: BasicNumberGenerator;
+
+    constructor() {
+        this.gen = new BasicNumberGenerator();
+        this.gen.minValue = 1;
+        this.gen.maxValue = 12;
+    }
+
+    createConfigurationElement(): HTMLElement {
+        var cfg: HTMLElement = document.createElement('div');
+        cfg.className = 'configuration table-division';
+        cfg.innerHTML = 'table division';
+
+        return cfg;
+    }
+
+    generateFact(): HTMLElement {
+        var fact: HTMLElement = document.createElement('div');
+        fact.className = 'fact-container table-division';
+
+        var result: number = this.gen.generate();
+        var right: number = this.gen.generate();
+        var left = result * right;
+
+        fact.innerHTML = "<span class='operand'>" + left + "</span>&divide;<span class='operand'>" + right + "</span>=<span class='result-placeholder'>&nbsp;</span>";
+        var rp: HTMLElement = <HTMLElement>fact.getElementsByClassName('result-placeholder')[0];
+        rp.style.width = '3em';
+        rp.style.display = 'inline-block';
+        return fact;
+    }
+}
--- MathFactGenerator/main.js
+++ MathFactGenerator/main.js
@@ -1,11 +1,12 @@
-﻿requirejs(['LongMultiplicationGenerator', 'GeneratorManager', 'TableMultiplicationGenerator'], function (lmg, gm, tmg) {
+﻿requirejs(['LongMultiplicationGenerator', 'GeneratorManager', 'TableMultiplicationGenerator', 'TableDivisionGenerator'], function (lmg, gm, tmg, tdg) {
     var el = document.getElementById('content');
     var f = document.getElementById('facts');
     var genBtn = document.getElementById('btn-generate');
 
     var m = new gm.GeneratorManager();
     m.addGenerator(new lmg.LongMultiplicationGenerator());
-    m.addGenerator(new tmg.TableMultiplicationGenerator())
+    m.addGenerator(new tmg.TableMultiplicationGenerator());
+    m.addGenerator(new tdg.TableDivisionGenerator());
     m.buildConfigurator();
 
     genBtn.onclick = () => { m.generateFacts(); };
