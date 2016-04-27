7644f9384895079f0cec758c763351984b6377bf
agr <andrey.grigoriev@gmail.com>
2016-04-26 19:45:40 -0700

    Extracted AdditionGenerator as a separate class for reuse

---
+++ MathFactGenerator/AdditionGenerator.ts
@@ -0,0 +1,69 @@
+﻿import { BasicNumberGenerator } from './BasicNumberGenerator';
+
+export class AdditionFact {
+    left: number;
+    right: number;
+    result: number;
+}
+
+export class AdditionGenerator {
+    leftOperandDescription: BasicNumberGenerator;
+    rightOperandDescription: BasicNumberGenerator;
+    resultDescription: BasicNumberGenerator;
+
+    constructor() {
+        this.leftOperandDescription = new BasicNumberGenerator();
+        this.rightOperandDescription = new BasicNumberGenerator();
+        this.resultDescription = new BasicNumberGenerator();
+    }
+
+    generateNumbers(): AdditionFact {
+        var allRestrictionsMet: boolean = false;
+        var r: AdditionFact = new AdditionFact();
+        this.leftOperandDescription.reset();
+        this.rightOperandDescription.reset();
+        this.resultDescription.reset();
+        if (this.leftOperandDescription.getCurrentMin() + this.rightOperandDescription.getCurrentMax() < this.resultDescription.getCurrentMin()) {
+            this.leftOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - this.rightOperandDescription.getCurrentMax();
+        }
+        if (this.leftOperandDescription.getCurrentMax() + this.rightOperandDescription.getCurrentMin() < this.resultDescription.getCurrentMin()) {
+            this.rightOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - this.leftOperandDescription.getCurrentMax();
+        }
+        if (this.leftOperandDescription.getCurrentMax() + this.rightOperandDescription.getCurrentMin() > this.resultDescription.getCurrentMax()) {
+            this.leftOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - this.rightOperandDescription.getCurrentMin();
+        }
+        if (this.leftOperandDescription.getCurrentMin() + this.rightOperandDescription.getCurrentMax() > this.resultDescription.getCurrentMax()) {
+            this.rightOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - this.leftOperandDescription.getCurrentMin();
+        }
+
+        if (!(this.leftOperandDescription.canGenerate() && this.rightOperandDescription.canGenerate())) {
+            return null;
+        }
+
+        var failCount: number = 0;
+
+        do {
+            r.left = this.leftOperandDescription.generate();
+            // left + right >= result.min => right >= result.min - left => right.min = result.min - left
+            this.rightOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - r.left;
+            // left + right <= result.max => right <= result.max - left => right.max = result.max - left
+            this.rightOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - r.left;
+
+            if (!this.rightOperandDescription.canGenerate()) {
+                ++failCount;
+            }
+            else {
+                r.right = this.rightOperandDescription.generate();
+                r.result = r.left + r.right;
+                allRestrictionsMet = true;
+            }
+            this.rightOperandDescription.reset();
+        } while (!allRestrictionsMet && failCount < 100);
+
+        if (failCount >= 100) {
+            return null;
+        }
+
+        return r;
+    }
+}
--- MathFactGenerator/HorizontalAdditionRenderer.ts
+++ MathFactGenerator/HorizontalAdditionRenderer.ts
@@ -1,30 +1,27 @@
 ﻿import { FactGenerator } from './FactGenerator';
 import { BasicNumberGenerator } from './BasicNumberGenerator';
+import { AdditionGenerator, AdditionFact } from './AdditionGenerator';
 
 export class HorizontalAdditionRenderer implements FactGenerator {
-    leftOperandDescription: BasicNumberGenerator;
-    rightOperandDescription: BasicNumberGenerator;
-    resultDescription: BasicNumberGenerator;
+    gen: AdditionGenerator;
 
     constructor() {
-        this.leftOperandDescription = new BasicNumberGenerator();
-        this.rightOperandDescription = new BasicNumberGenerator();
-        this.resultDescription = new BasicNumberGenerator();
+        this.gen = new AdditionGenerator();
     }
 
     createConfigurationElement(): HTMLElement {
         var tle: HTMLElement = document.createElement('div');
         tle.className = 'configuration horizontal-addition';
         tle.innerHTML = '<div class="caption">Horizontal addition</div>';
-        tle.appendChild(this.leftOperandDescription.createConfigurationElement('Left operand'));
-        tle.appendChild(this.rightOperandDescription.createConfigurationElement('Right operand'));
-        tle.appendChild(this.resultDescription.createConfigurationElement('Result'));
+        tle.appendChild(this.gen.leftOperandDescription.createConfigurationElement('Left operand'));
+        tle.appendChild(this.gen.rightOperandDescription.createConfigurationElement('Right operand'));
+        tle.appendChild(this.gen.resultDescription.createConfigurationElement('Result'));
 
         return tle;
     }
 
     generateFact(): HTMLElement {
-        var n: FactNumbers = this.generateNumbers();
+        var n: AdditionFact = this.gen.generateNumbers();
 
         if (n === null) {
             return null;
@@ -38,60 +35,4 @@ export class HorizontalAdditionRenderer implements FactGenerator {
         rp.style.display = 'inline-block';
         return fact;
     }
-
-    private generateNumbers(): FactNumbers {
-        var allRestrictionsMet: boolean = false;
-        var r: FactNumbers = new FactNumbers();
-        this.leftOperandDescription.reset();
-        this.rightOperandDescription.reset();
-        this.resultDescription.reset();
-        if (this.leftOperandDescription.getCurrentMin() + this.rightOperandDescription.getCurrentMax() < this.resultDescription.getCurrentMin()) {
-            this.leftOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - this.rightOperandDescription.getCurrentMax();
-        }
-        if (this.leftOperandDescription.getCurrentMax() + this.rightOperandDescription.getCurrentMin() < this.resultDescription.getCurrentMin()) {
-            this.rightOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - this.leftOperandDescription.getCurrentMax();
-        }
-        if (this.leftOperandDescription.getCurrentMax() + this.rightOperandDescription.getCurrentMin() > this.resultDescription.getCurrentMax()) {
-            this.leftOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - this.rightOperandDescription.getCurrentMin();
-        }
-        if (this.leftOperandDescription.getCurrentMin() + this.rightOperandDescription.getCurrentMax() > this.resultDescription.getCurrentMax()) {
-            this.rightOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - this.leftOperandDescription.getCurrentMin();
-        }
-
-        if (!(this.leftOperandDescription.canGenerate() && this.rightOperandDescription.canGenerate())) {
-            return null;
-        }
-
-        var failCount: number = 0;
-
-        do {
-            r.left = this.leftOperandDescription.generate();
-            // left + right >= result.min => right >= result.min - left => right.min = result.min - left
-            this.rightOperandDescription.tempMinValue = this.resultDescription.getCurrentMin() - r.left;
-            // left + right <= result.max => right <= result.max - left => right.max = result.max - left
-            this.rightOperandDescription.tempMaxValue = this.resultDescription.getCurrentMax() - r.left;
-
-            if (!this.rightOperandDescription.canGenerate()) {
-                ++failCount;
-            }
-            else {
-                r.right = this.rightOperandDescription.generate();
-                r.result = r.left + r.right;
-                allRestrictionsMet = true;
-            }
-            this.rightOperandDescription.reset();
-        } while (!allRestrictionsMet && failCount < 100);
-
-        if (failCount >= 100) {
-            return null;
-        }
-
-        return r;
-    }
-}
-
-class FactNumbers {
-    left: number;
-    right: number;
-    result: number;
 }
